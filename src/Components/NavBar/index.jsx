c5b84b85b38958ad4b05a86b8d2ac9f11c148fc2
Facundo <oseirafacundo@gmail.com>
2023-04-11 21:30:10 -0300

    preentrega1

---
+++ src/Components/NavBar/index.jsx
@@ -0,0 +1,27 @@
+ 
+  import React from 'react';
+  
+  function NavBar() {
+    return (
+      <div className="container">
+        <nav className="nav">
+            <div className="nav_logo">
+                <a className="nav_brand" href="#">FACUNDO</a>
+            </div>
+          <ul className="nav-list">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Productos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Catalogos</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+}
+
+export default NavBar;
